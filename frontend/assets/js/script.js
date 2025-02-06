05bbc0eaae0f098fb1044ae91077285dd1a3cf26
kitarani <123284445+kitarani@users.noreply.github.com>
2025-02-07 04:40:06 +0500

    Update script.js

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,35 +1,64 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle) {
+  navToggle.addEventListener("click", () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose) {
+  navClose.addEventListener("click", () =>{
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 /*=============== SHOW CART ===============*/
-
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
-
-
+if(cartShop) {
+  cartShop.addEventListener("click", () =>{
+    cart.classList.add('show-cart')
+  })
+}
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
-
+if(cartClose) {
+  cartClose.addEventListener("click", () =>{
+    cart.classList.remove('show-cart')
+  })
+}
 
 /*=============== SHOW LOGIN ===============*/
-
+const login = document.getElementById('login'),
+      loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close')
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
-
+if(loginButton) {
+  loginButton.addEventListener("click", () =>{
+    login.classList.add('show-login')
+  })
+}
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
-
+if(loginClose) {
+  loginClose.addEventListener("click", () =>{
+    login.classList.remove('show-login')
+  })
+}
 
 /*=============== HOME SWIPER ===============*/
 var homeSwiper = new Swiper(".home-swiper", {
@@ -51,11 +80,24 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== NEW SWIPER ===============*/
+var newSwiper = new Swiper(".new-swiper", {
+  spaceBetween: 16,
+  slidesPerView: "auto",
+  centeredSlides:true,
+  loop:'true',
 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+});
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
-
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 /*=============== LIGHT BOX ===============*/
 
 
