8d803e28b01fe5c24d8e2e9e1a315f543457ae5c
kitarani <123284445+kitarani@users.noreply.github.com>
2025-02-09 05:39:48 +0500

    Update script.js

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -102,6 +102,32 @@ window.addEventListener('scroll', scrollUp)
 
 
 /*=============== QUESTIONS ACCORDION ===============*/
+const accordionItem = document.querySelectorAll('.questions__item')
 
+accordionItem.forEach((item) => {
+  const accordionHeader = item.querySelector('.questions__header')
 
-/*=============== STYLE SWITCHER ===============*/
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open')
+
+    toggleItem(item)
+
+    if(openItem && openItem !== item) {
+      toggleItem(openItem)
+    }
+  })
+})
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.questions__content')
+
+  if(item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  }
+  else{
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+}
+
