0d224d99a7044de7374b2e08d454a9d83a9fbbc0
kitarani <erkezhan211205@gmail.com>
2025-02-12 11:20:21 +0500

    Удалены все файлы

--- backend/models/category.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Category struct {
-	gorm.Model
-	Name     string    `json:"name" gorm:"not null;unique"`
-	Products []Product `json:"products" gorm:"foreignKey:CategoryID"`
-}
--- backend/models/order.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Order struct {
-	gorm.Model
-	UserID uint   `json:"user_id"`
-	Status string `json:"status" gorm:"default:'pending'"`
-}
